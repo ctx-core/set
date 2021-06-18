87022896433b53d1a2c9f76fe05ecabd36758344
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export * from './difference_set_';
-export * from './intersection_set_';
-export * from './union_set_';
-export * from './set_member_T';
-//# sourceMappingURL=src/index.js.map
--- src/set_member_T.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=src/set_member_T.js.map
