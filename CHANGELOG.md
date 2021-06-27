48bd29d06fc022c2dae12af2cb36459f2af02e2e
Brian Takita <brian.takita@gmail.com>
2021-06-26 23:07:17 -0400

    ## 11.0.11
    ### Patch Changes
    
    - dist directory
    - Updated dependencies [undefined]
    - @ctx-core/function@19.1.10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # @ctx-core/set
 
+## 11.0.11
+
+### Patch Changes
+
+- dist directory
+- Updated dependencies [undefined]
+  - @ctx-core/function@19.1.10
+
 ## 11.0.10
 
 ### Patch Changes
--- src/difference_set_.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns a new set with a _difference of the array-like arguments.
- * @see {@link http://www.2ality.com/2015/01/es6-set-operations.html}
- */
-export declare function difference_set_<I extends unknown = unknown>(member_a_nowrap_a_nowrap: I): Set<I>;
-export { difference_set_ as _difference, difference_set_ as set_difference_, difference_set_ as _difference__set, };
