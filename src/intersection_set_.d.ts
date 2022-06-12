dd114d300cd7fdff0cee77cbc12ad05ea1217213
Brian Takita <brian.takita@gmail.com>
2022-06-12 13:22:50 -0500

    difference_set_,intersection_set_,union_set_: fix: argument type is Set<I>

--- src/intersection_set_.d.ts
+++ src/intersection_set_.d.ts
@@ -3,7 +3,7 @@ import { a_nowrap_T } from '@ctx-core/function'
  * Returns a `set` with the _intersection of the members
  * @see {@link http://www.2ality.com/2015/01/es6-set-operations.html}
  */
-export declare function intersection_set_<I extends unknown = unknown>(member_a_nowrap_a_nowrap:a_nowrap_T<I>):Set<I>;
+export declare function intersection_set_<I extends unknown = unknown>(member_a_nowrap_a_nowrap:a_nowrap_T<Set<I>>):Set<I>;
 export {
 	intersection_set_ as set_intersection_fn,
 	intersection_set_ as _intersection,
--- src/union_set_.d.ts
+++ src/union_set_.d.ts
@@ -2,5 +2,5 @@
  * Returns a `set` with the _union of the members
  * @see {@link http://www.2ality.com/2015/01/es6-set-operations.html}
  */
-export declare function union_set_<I extends unknown = unknown>(member_a_nowrap_a_nowrap:I):Set<I>;
+export declare function union_set_<I extends unknown = unknown>(member_a_nowrap_a_nowrap:Set<I>):Set<I>;
 export { union_set_ as _union, union_set_ as set_union_fn, union_set_ as _union__set, }
